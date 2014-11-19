00b39323490b85655471c934f8d7d4af5e3f7beb
djarecka <djarecka@gmail.com>
2014-11-19 20:17:37 +0100

    work in progress on initialisation of superdroplets in micro_step

--- kid.py
+++ kid.py
@@ -2,16 +2,63 @@
 
 import numpy as np
 import cffi
+import libcloudphxx as libcl
+
 ffi = cffi.FFI()
 
-real_t = np.float64
+# object storing super-droplet model state (to be initialised)
+prtcls = False
+
+# dictionary of simulation parameters
+params = {
+  "real_t" : np.float64,
+  "backend" : libcl.lgrngn.backend_t.serial,
+  "sd_conc" : 128,
+  "kappa" : .61,
+  "meanr" : .04e-6,
+  "gstdv" : 1.4,
+  "n_tot" : 100e6
+}
+
+def lognormal(lnr):
+  from math import exp, log, sqrt, pi
+  print lnr
+  return params["n_tot"] * exp(
+    -pow((lnr - log(params["meanr"])), 2) / 2 / pow(log(params["gstdv"]),2)
+  ) / log(params["gstdv"]) / sqrt(2*pi);
 
 @ffi.callback("void(int, int, int, double*, double*, double*, double*, double*)")
 def micro_step(it_diag, size_z, size_x, th_ar, qv_ar, rho_ar, uh_ar, wh_ar):
-  print "micro_step from Python", th_ar, size_z, size_x
-  print "i_dgtime z fortrana", it_diag
-  array = np.frombuffer(ffi.buffer(th_ar, size_x*size_z*np.dtype(real_t).itemsize), dtype=real_t)
+  global prtcls
+
+  print "in python::micro_step() from Python"#, prtcls #, th_ar, size_z, size_x
+
+  # superdroplets: initialisation (done only once)
+  if not prtcls:
+    print "initialisation!"
+    opts_init = libcl.lgrngn.opts_init_t()
+
+    opts_init.sd_conc = params["sd_conc"]
+    opts_init.dry_distros = { params["kappa"] : lognormal }
+
+    prtcls = libcl.lgrngn.factory(params["backend"], opts_init)
+    #prtcls.init()
+
+  # mapping local NumPy arrays to the Fortran data locations
+  # TODO: halo!, strides
+  th = np.frombuffer(
+    ffi.buffer(th_ar, size_x*size_z*np.dtype(params["real_t"]).itemsize), 
+    dtype=params["real_t"]
+  )
+
+  # superdroplets: all what have to be done within a timestep
+  #prtcls.step_sync()
+  #prtcls.step_async()
+
+  #print "i_dgtime z fortrana", it_diag
   #print array.reshape((size_x,size_z))[1:-1,:]
+
+
 
 # C functions
 ffi.cdef("void save_ptr(char*,void*);")
