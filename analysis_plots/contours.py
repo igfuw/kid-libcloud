1073a41290154ff3211f2ef63d5fcbd5e6aa6845
djarecka <djarecka@gmail.com>
2014-12-23 19:06:29 +0100

    adding a simple contour plots for list of it

---
+++ analysis_plots/contours.py
@@ -0,0 +1,54 @@
+from scipy.io import netcdf
+import numpy as np
+# for skua - TODO
+#import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+import matplotlib.ticker as ticker
+import pdb
+
+# names of variable to plot
+Variable_name_l = ["theta", "vapor", "RH", "RH_lib_post_cond", "T_lib_post_cond", "w", "dtheta_mphys", "dqv_mphys"]
+
+# reading variables from the netcdf file
+def reading_netcdf(file_r, var_l):
+    var_d = {}
+    nf = netcdf.netcdf_file(file_r, 'r')
+    for var in var_l + ["z", "x", "time"]:
+        var_d[var] = nf.variables[var][:]
+    return var_d
+
+
+def contour_plot(var_name_l, var_d, it):
+    plt.figure(1, figsize = (8,8))
+    x_range = var_d["x"][:-1]
+    z_range = var_d["z"][:-1]
+    X, Y = np.meshgrid(x_range, z_range)
+    nr_pl = 1
+    for var in var_name_l:
+        print var
+        ax = plt.subplot(2,2,nr_pl)
+        #legend = []
+        #legend.append("time = " + str(var_d["time"][it]))
+        var_domain = var_d[var][:-1,:-1,it]
+        CS = plt.contourf(X, Y, var_domain,  cmap=plt.cm.Blues, alpha=0.7)
+        nr_pl += 1
+        plt.xlabel(var, fontsize=10)
+        plt.ylabel(r'height $[m]$', fontsize=10)
+        #plt.legend(legend, prop = FontProperties(size=8))
+
+    plt.savefig("contour_" + str(var_d["time"][it]) + "-".join(var_name_l) + ".pdf")
+    plt.show()
+
+
+
+def main(filename, variable_name_l=Variable_name_l, it_l=[0,-1]):
+    var_d = reading_netcdf(filename, variable_name_l)
+    variable_name_lpl = [variable_name_l[i:i+4] for i in xrange(0, len(variable_name_l), 4)]
+    for it in it_l:
+        for var_name in variable_name_lpl:
+            contour_plot(var_name, var_d, it)
+
+
+main("../kid_a_setup/output/SC_2D_out.nc")
