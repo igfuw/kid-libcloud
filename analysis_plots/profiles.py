9fd13eb9729ee7688f1048df07f29ac7054615be
djarecka <djarecka@gmail.com>
2015-01-09 18:29:25 -0700

    work on argument parser

--- analysis_plots/profiles.py
+++ analysis_plots/profiles.py
@@ -6,10 +6,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 import matplotlib.ticker as ticker
+from argparse import ArgumentParser
 import pdb
 
 # names of variable to plot
 Variable_name_l = ["theta", "vapor", "RH", "RH_lib_post_cond", "T_lib_post_cond", "w", "dtheta_mphys", "dqv_mphys",  "cloud_number_r20um", "rain_number_r20um", "cloud_mass_r20um", "rain_mass_r20um"]
+
+prsr = ArgumentParser(add_help=True, description='TODO')
+
+prsr.add_argument('--outdir', required=True, help='output directory')
+
+args = prsr.parse_args()
 
 # reading variables from the netcdf file
 def reading_netcdf(netcdf_file, var_l):
@@ -29,9 +36,10 @@ def plotting_profiles(var_name_l, var_d):
         for it in range(0, var_d[var].shape[0], 4):
             legend.append("time = " + str(var_d["time"][it]))
             var_hor_av = np.mean(var_d[var][it,:,:], axis=1)
-            print var_hor_av
+            print "min, max", var_hor_av.min(), var_hor_av.max()
             ax.plot(var_hor_av[:-1], var_d["z"][:-1]) #TODO: -999 in the last place, using missing arrays
         nr_pl += 1
+        pdb.set_trace()
         ax.set_xlim(var_d[var][1:,:-1,:].min(), var_d[var][1:,:-1:,:].max())
         print len(ax.xaxis.get_major_ticks())
         for i, tick in enumerate(ax.xaxis.get_major_ticks()):
@@ -56,4 +64,4 @@ def main(filename, variable_name_l=Variable_name_l):
         plotting_profiles(var_name, var_d)
 
 
-main("../kid_a_setup/output/SC_2D_out.nc")
+main("../kid_a_setup/output/output_nocoal_nosed_5sstp/SC_2D_out.nc")
