1a9f9c0ea0649a7213972c854ffdb2c92cc748b1
djarecka <djarecka@gmail.com>
2015-01-20 16:56:25 -0700

    adding command line arguments - outdir and it_l

--- analysis_plots/contours.py
+++ analysis_plots/contours.py
@@ -6,10 +6,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 import matplotlib.ticker as ticker
+from argparse import ArgumentParser
+import os
 import pdb
 
 # names of variable to plot
 Variable_name_l = ["theta", "vapor", "RH", "RH_lib_post_cond", "T_lib_post_cond", "w", "dtheta_mphys", "dqv_mphys", "cloud_number_r20um", "rain_number_r20um", "cloud_mass_r20um", "rain_mass_r20um"]
+
+prsr = ArgumentParser(add_help=True, description='TODO')
+prsr.add_argument('--outdir', default="", help='output directory from kid_a_setup/output')
+prsr.add_argument('--it_l', nargs='+', type=int, required=None, help='time indexes for plotting, e.g., --it_l 0 2 -1 ')
+args = prsr.parse_args()
+
 
 # reading variables from the netcdf file
 def reading_netcdf(netcdf_file, var_l):
@@ -19,7 +27,7 @@ def reading_netcdf(netcdf_file, var_l):
     return var_d
 
 
-def contour_plot(var_name_l, var_d, it, nr_fig):
+def contour_plot(outdir_path, var_name_l, var_d, it, nr_fig):
     plt.figure(nr_fig, figsize = (8,8))
     x_range = var_d["x"][:-1]
     z_range = var_d["z"][:-1]
@@ -43,19 +51,21 @@ def contour_plot(var_name_l, var_d, it, nr_fig):
         plt.ylabel(r'height $[m]$', fontsize=10)
 #plt.legend(legend, prop = FontProperties(size=8))
 
-    plt.savefig("contour_" + str(var_d["time"][it]) + "-".join(var_name_l) + ".pdf")
+    plt.savefig(os.path.join(outdir_path, "contour_" + str(var_d["time"][it]) + "-".join(var_name_l) + ".pdf"))
     plt.show()
 
 
 
-def main(filename, variable_name_l=Variable_name_l, it_l=[0, 12, 24, -1]):
-    nf = netcdf.netcdf_file(filename, 'r')
+def main(outdir_path, filename="SC_2D_out.nc", variable_name_l=Variable_name_l):
+    nf = netcdf.netcdf_file(os.path.join(outdir_path, filename), 'r')
     var_d = reading_netcdf(nf, variable_name_l)
     variable_name_lpl = [variable_name_l[i:i+4] for i in xrange(0, len(variable_name_l), 4)]
+    it_l = args.it_l if args.it_l!=None else range(0, var_d["time"].shape[0], 12)
     nr_fig = 1
     for it in it_l:
         for var_name in variable_name_lpl:
-            contour_plot(var_name, var_d, it, nr_fig)
+            contour_plot(outdir_path, var_name, var_d, it, nr_fig)
             nr_fig += 1
 
-main("../kid_a_setup/output/SC_2D_out.nc")
+main(os.path.join("../kid_a_setup/output", args.outdir))
+
--- analysis_plots/profiles.py
+++ analysis_plots/profiles.py
@@ -7,15 +7,15 @@ import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 import matplotlib.ticker as ticker
 from argparse import ArgumentParser
+import os
 import pdb
 
 # names of variable to plot
 Variable_name_l = ["theta", "vapor", "RH", "RH_lib_post_cond", "T_lib_post_cond", "w", "dtheta_mphys", "dqv_mphys",  "cloud_number_r20um", "rain_number_r20um", "cloud_mass_r20um", "rain_mass_r20um"]
 
 prsr = ArgumentParser(add_help=True, description='TODO')
-
-prsr.add_argument('--outdir', required=True, help='output directory')
-
+prsr.add_argument('--outdir', default="", help='output directory from kid_a_setup/output')
+prsr.add_argument('--it_l', nargs='+', type=int, required=None, help='time indexes for plotting, e.g., --it_l 0 2 -1 ')
 args = prsr.parse_args()
 
 # reading variables from the netcdf file
@@ -26,20 +26,19 @@ def reading_netcdf(netcdf_file, var_l):
     return var_d
 
 
-def plotting_profiles(var_name_l, var_d):
+def plotting_profiles(outdir_path, var_name_l, var_d, it_plot_l):
     plt.figure(1, figsize = (8,8))
     nr_pl = 1
     for var in var_name_l:
         print var
         ax = plt.subplot(2,2,nr_pl)
         legend = []
-        for it in range(0, var_d[var].shape[0], 4):
+        for it in it_plot_l:
             legend.append("time = " + str(var_d["time"][it]))
             var_hor_av = np.mean(var_d[var][it,:,:], axis=1)
             print "min, max", var_hor_av.min(), var_hor_av.max()
             ax.plot(var_hor_av[:-1], var_d["z"][:-1]) #TODO: -999 in the last place, using missing arrays
         nr_pl += 1
-        pdb.set_trace()
         ax.set_xlim(var_d[var][1:,:-1,:].min(), var_d[var][1:,:-1:,:].max())
         print len(ax.xaxis.get_major_ticks())
         for i, tick in enumerate(ax.xaxis.get_major_ticks()):
@@ -51,17 +50,18 @@ def plotting_profiles(var_name_l, var_d):
         plt.ylabel(r'height $[m]$', fontsize=10)
         plt.legend(legend, prop = FontProperties(size=8))
 
-    plt.savefig("profiles_" + "-".join(var_name_l) + ".pdf")
+    plt.savefig(os.path.join(outdir_path, "profiles_" + "-".join(var_name_l) + ".pdf"))
     plt.show()
 
 
 
-def main(filename, variable_name_l=Variable_name_l):
-    nf = netcdf.netcdf_file(filename, 'r')
+def main(outdir_path, filename="SC_2D_out.nc", variable_name_l=Variable_name_l):
+    nf = netcdf.netcdf_file(os.path.join(outdir_path, filename), 'r')
     var_d = reading_netcdf(nf, variable_name_l)
     variable_name_lpl = [variable_name_l[i:i+4] for i in xrange(0, len(variable_name_l), 4)]
+    it_l = args.it_l if args.it_l!=None else range(0, var_d["time"].shape[0], 4)
     for var_name in variable_name_lpl:
-        plotting_profiles(var_name, var_d)
+        plotting_profiles(outdir_path, var_name, var_d, it_plot_l=it_l)
 
 
-main("../kid_a_setup/output/output_nocoal_nosed_5sstp/SC_2D_out.nc")
+main(os.path.join("../kid_a_setup/output", args.outdir))
