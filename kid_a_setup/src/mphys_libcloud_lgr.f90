3caa43a4e9825968e1314e965aea56ae268f680f
slayoo <slayoo@igf.fuw.edu.pl>
2015-03-04 20:11:08 +0100

    removing debug messages

--- kid_a_setup/src/mphys_libcloud_lgr.f90
+++ kid_a_setup/src/mphys_libcloud_lgr.f90
@@ -62,7 +62,6 @@ contains
       call c_f_procpointer(cptr, fptr)
     end if
 
-print*, "Fortran:", z
     ! do the below every timestep
     if (.not. fptr(i_dgtime, dt, nz, nx+2 , &
                    theta, qv, rho, rho_half, & 
