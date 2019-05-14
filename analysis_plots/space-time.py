381cfd2534933f975e3e19533d779a69f104f455
pdziekan <pdziekanp@gmail.com>
2019-05-14 16:05:39 +0200

    space-time plot fix

--- analysis_plots/space-time.py
+++ analysis_plots/space-time.py
@@ -29,10 +29,10 @@ def reading_netcdf(netcdf_file, var_l):
     # comparing with _ante_cond , because post_cond include change in theta due to cond, which is not included in KiD calculations
     # also, for tihs comparison to be correct, save_diag in KiD has to be called before applying mphys and adve dtheta/dqv (i.e. before step_column)
     # but then all the diags in KiD are "pre step", which is not the default behaviour
-    var_d["RH_diff"] = -(netcdf_file.variables["RH"][:] - netcdf_file.variables["RH_lib_ante_cond"][:])
-    var_d["T_diff"] = -(netcdf_file.variables["temperature"][:] - netcdf_file.variables["T_lib_ante_cond"][:])# / netcdf_file.variables["temperature"][:]
-    var_d["p_diff"] = -(netcdf_file.variables["pressure"][:] - netcdf_file.variables["pressure_lib_ante_cond"][:]/1e4)# / netcdf_file.variables["pressure"][:]
-    var_d["psat_diff"] = -(netcdf_file.variables["psat"][:] - netcdf_file.variables["psat_lib_formula_ante_cond"][:]/1e2)# / netcdf_file.variables["psat"][:]
+    #var_d["RH_diff"] = -(netcdf_file.variables["RH"][:] - netcdf_file.variables["RH_lib_ante_cond"][:])
+    #var_d["T_diff"] = -(netcdf_file.variables["temperature"][:] - netcdf_file.variables["T_lib_ante_cond"][:])# / netcdf_file.variables["temperature"][:]
+    #var_d["p_diff"] = -(netcdf_file.variables["pressure"][:] - netcdf_file.variables["pressure_lib_ante_cond"][:]/1e4)# / netcdf_file.variables["pressure"][:]
+    #var_d["psat_diff"] = -(netcdf_file.variables["psat"][:] - netcdf_file.variables["psat_lib_formula_ante_cond"][:]/1e2)# / netcdf_file.variables["psat"][:]
     for var in var_l + ["z", "x", "time"]:
         var_d[var] = netcdf_file.variables[var][:]
     return var_d
@@ -75,7 +75,7 @@ def contour_plot(outdir_path, var_name_l, var_d, nr_fig):
 def main(outdir_path, filename="1D_out.nc", variable_name_l=Variable_name_l):
     nf = netcdf.netcdf_file(os.path.join(outdir_path, filename), 'r')
     var_d = reading_netcdf(nf, variable_name_l)
-    variable_name_l  += ["RH_diff", "T_diff", "p_diff", "psat_diff"]
+    #variable_name_l  += ["RH_diff", "T_diff", "p_diff", "psat_diff"]
     variable_name_lpl = [variable_name_l[i:i+4] for i in xrange(0, len(variable_name_l), 4)]
     nr_fig = 1
     for var_name in variable_name_lpl:
